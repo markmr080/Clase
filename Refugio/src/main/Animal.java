8dfaa6159d462e19a13a9fa74e3741172bd8d926
markmr080 <markmr080@gmail.com>
2025-10-04 04:00:06 +0200

    a

---
+++ Refugio/src/main/Animal.java
@@ -0,0 +1,29 @@
+package main;
+
+
+
+public class Animal {
+	public String nombre;
+	public int edad;
+
+
+	Animal (String nombre, int edad) {
+		this.nombre=nombre;
+		this.edad = edad;
+		
+		
+		
+	}
+	
+	public void mostrarDatos() {
+	
+		System.out.print(nombre + " tiene " + edad + " años");	
+	}
+	
+	@Override
+	public String toString() {
+		return "nombre=" + nombre + ", edad=" + edad;
+	}
+
+
+}
---
+++ Refugio/src/main/Perro.java
@@ -0,0 +1,40 @@
+package main;
+
+public class Perro extends Animal{
+	
+	
+	private String raza;
+
+	Perro(String nombre, int edad, String raza) {
+		super(nombre, edad);
+		// TODO Auto-generated constructor stub
+		this.raza = raza;
+	}
+	
+	
+	public void ladrar() {
+		
+		System.out.println("Guau guau");
+		
+	}
+
+
+	@Override
+	public void mostrarDatos() {
+		
+		super.mostrarDatos();
+		System.out.println(" y raza " + raza);
+		
+	}
+	
+
+
+	
+
+
+	
+	
+	
+	
+
+}
